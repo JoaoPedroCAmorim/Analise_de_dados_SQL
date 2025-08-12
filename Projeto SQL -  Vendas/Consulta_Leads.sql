dd1fcbce2b4f0fd5e9dd19605ce48f3e042d7dcd
João Pedro <jcunhaamorim@gmail.com>
2025-08-11 22:56:07 -0300

    Create Consulta_Leads.sql

---
+++ Projeto SQL -  Vendas/Consulta_Leads.sql
@@ -0,0 +1,8 @@
+leads as (
+		select
+			date_trunc('month', visit_page_date)::date as visit_page_month,
+			count(*) as visit_page_count
+		from sales.funnel
+		group by visit_page_month
+		order by visit_page_month
+	)
