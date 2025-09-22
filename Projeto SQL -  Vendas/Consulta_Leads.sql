d1ad63fc45b2b9f2e2548396875a7c012f8b18fd
João Pedro da Cunha Amorim <jcunhaamorim@gmail.com>
2025-09-21 21:57:39 -0300

    Delete Projeto SQL -  Vendas directory

--- Projeto SQL -  Vendas/Consulta_Leads.sql
+++
@@ -1,8 +0,0 @@
-leads as (
-		select
-			date_trunc('month', visit_page_date)::date as visit_page_month,
-			count(*) as visit_page_count
-		from sales.funnel
-		group by visit_page_month
-		order by visit_page_month
-	)
